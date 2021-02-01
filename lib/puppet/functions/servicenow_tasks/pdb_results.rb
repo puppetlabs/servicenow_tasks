efe434a9bd4027e2c2ccbff3e8e90daa4fe572f0
gregleesparks@gmail.com <gregleesparks@gmail.com>
2021-02-01 13:37:32 -0800

    (PIE-498) Add pdb query plan
    This adds a plan that runs the pdb query independently. It uses the
    results to create a CI in ServiceNow.

--- lib/puppet/functions/servicenow_tasks/pdb_results.rb
+++ lib/puppet/functions/servicenow_tasks/pdb_results.rb
@@ -6,28 +6,26 @@ Puppet::Functions.create_function(:'servicenow_tasks::pdb_results') do
   end
 
   def pdb_results(node)
-    fact_map = {
-      # PuppetDB fact => ServiceNow CI field
-      'fqdn'                   => 'fqdn',
-      'domain'                 => 'dns_domain',
-      'serialnumber'           => 'serial_number',
-      'operatingsystemrelease' => 'os_version',
-      'physicalprocessorcount' => 'cpu_count',
-      'processorcount'         => 'cpu_core_count',
-      'processors.models.0'    => 'cpu_type',
-      'memorysize_mb'          => 'ram',
-      'is_virtual'             => 'virtual',
-      'macaddress'             => 'mac_address',
-    }
+   fact_map = {
+     # PuppetDB fact => ServiceNow CI field
+     'fqdn'                   => 'fqdn',
+     'domain'                 => 'dns_domain',
+     'serialnumber'           => 'serial_number',
+     'operatingsystemrelease' => 'os_version',
+     'physicalprocessorcount' => 'cpu_count',
+     'processorcount'         => 'cpu_core_count',
+     'processors.models.0'    => 'cpu_type',
+     'memorysize_mb'          => 'ram',
+     'is_virtual'             => 'virtual',
+     'macaddress'             => 'mac_address',
+   }
 
-    fact_query_filter = []
-    fact_map.keys.each do |fact|
-      fact_name = fact.split('.')[0]
-      fact_query_filter.push("name='#{fact_name}'")
-    end
-    query = "facts[name,value] { (#{fact_query_filter.join(' or ')}) and certname = '#{node}' }"
-    puppetdb_client = Puppet.lookup(:bolt_pdb_client)
-    # Query PuppetDB
-    puppetdb_client.make_query(query).to_json
+   fact_query_filter = []
+   fact_map.each do |fact, _field|
+     fact_name = fact.split('.')[0]
+     fact_query_filter.push("name='#{fact_name}'")
+   end
+   
+   "facts[name,value] { (#{fact_query_filter.join(' or ')}) and certname = '#{node}' }"
   end
 end
